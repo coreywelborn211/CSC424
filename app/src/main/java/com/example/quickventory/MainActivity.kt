dccb0b6a6c6e63bacfa16440a8e09adf3c040646
Corey Welborn <coreywelborn@yahoo.com>
2021-03-11 17:16:26 -0600

    Added Navigation, Login, and Inventory List

--- app/src/main/java/com/example/quickventory/MainActivity.kt
+++ app/src/main/java/com/example/quickventory/MainActivity.kt
@@ -1,14 +1,59 @@
 package com.example.quickventory
 
+
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import com.example.quickventory.home.HomeFragment
+import com.example.quickventory.items.ItemsFragment
+import com.example.quickventory.login.LoginFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
-
         setContentView(R.layout.activity_main)
+
+        val homeFragment = HomeFragment()
+        val itemsFragment = ItemsFragment()
+        val loginFragment = LoginFragment()
+
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        var searchImage: ImageView = findViewById(R.id.searchImage)
+
+        val sharedPref = getSharedPreferences("token", Context.MODE_PRIVATE)
+        val theID = sharedPref.getInt("TOKEN", -1)
+
+        if(theID == -1){
+            bottomNavigation.setVisibility(View.GONE)
+            searchImage.setVisibility(View.GONE)
+            makeCurrentFragment(loginFragment)
+        } else {
+            bottomNavigation.setVisibility(View.VISIBLE)
+            searchImage.setVisibility(View.VISIBLE)
+            makeCurrentFragment(homeFragment)
+        }
+
+        bottomNavigation.selectedItemId = R.id.menuHome
+
+        bottomNavigation.setOnNavigationItemSelectedListener{
+            when(it.itemId){
+                R.id.menuHome -> makeCurrentFragment(homeFragment)
+                R.id.menuItems -> makeCurrentFragment(itemsFragment)
+                R.id.menuScan -> makeCurrentFragment(loginFragment)
+            }
+            true
+        }
+
+    }
+
+    private fun makeCurrentFragment(fragment: Fragment) = supportFragmentManager.beginTransaction().apply {
+        replace(R.id.wrapper, fragment)
+        commit()
     }
 }
